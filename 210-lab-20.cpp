15724eda8bce787e3be9589bb13b0e2f60f4e498
Dainiz Almazan Bustos <85927389+dainizzz@users.noreply.github.com>
2025-10-06 18:50:29 -0700

    10-minute update

--- 210-lab-20.cpp
+++ 210-lab-20.cpp
@@ -12,8 +12,7 @@ public:
     // constructors
     Chair() {
         prices = new double[SIZE];
-        // TODO: randomly select 3 or 4 num legs
-        legs = rand() % 2 + 3;
+        legs = rand() % (LEG_MAX - LEG_MIN + 1) + LEG_MIN;
         for (int i = 0; i < SIZE; i++)
             prices[i] = (rand() % (MAX-MIN+1) + MIN) / (double) 100;
     }
@@ -67,15 +66,10 @@ int main() {
     delete livingChair;
     livingChair = nullptr;
 
-    // TODO: Amend so the default constructors are used to populate these objects
     //creating dynamic array of chair objects
+    // The default constructor randomly assigns a value for legs and prices,
     Chair *collection = new Chair[SIZE];
-    collection[0].setLegs(4);
-    collection[0].setPrices(441.41, 552.52, 663.63);
-    collection[1].setLegs(4);
-    collection[1].setPrices(484.84, 959.59, 868.68);
-    collection[2].setLegs(4);
-    collection[2].setPrices(626.26, 515.15, 757.57);
+
     for (int i = 0; i < SIZE; i++)
         collection[i].print();
     
